d2b023b15b856869919f23715f69d6430ecc00b5
jascha-riebler <jascha@riebler>
2018-05-15 17:16:40 +0200

    -

--- app/src/main/java/riebler/jascha/nim_spiel_android/MainActivity.java
+++ app/src/main/java/riebler/jascha/nim_spiel_android/MainActivity.java
@@ -1,13 +1,21 @@
 package riebler.jascha.nim_spiel_android;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mainViewPager;
+    private LinearLayout mainLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainViewPager = (ViewPager) findViewById(R.id.mainViewPager);
+        mainLinearLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
     }
 }
