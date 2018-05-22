a333f67b18f61496219a51086e534cb40b4ab628
jascha-riebler <jascha@riebler.de>
2018-05-22 19:57:24 +0200

    Nim(Game) UI

--- app/src/main/java/riebler/jascha/nim_spiel_android/Nimfun.java
+++ app/src/main/java/riebler/jascha/nim_spiel_android/Nimfun.java
@@ -8,6 +8,6 @@ public class Nimfun extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nimfun);
+        setContentView(R.layout.activity_nim);
     }
 }
